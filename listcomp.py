f6d170b8b0570d7be5e56553696e12b0d4f00b5a
michelamar <mmarchini@stuy.edu>
2018-05-02 14:50:56 -0400

    not working

--- listcomp.py
+++ listcomp.py
@@ -1,4 +1,8 @@
 def check_pass(password):
-    
+    return ((len([char for char in password if char.islower()]) > 0) and (len([char for char in password if char.isupper()]) > 0) and (len([char for char in password if char.isdigit()]) > 0))
 
-def strength(password):
+print 'pizza: ' + str(check_pass('pizza'))
+print 'Pizza: ' + str(check_pass('pizza'))
+print 'P1zza: ' + str(check_pass('pizza'))
+
+#def strength(password):
